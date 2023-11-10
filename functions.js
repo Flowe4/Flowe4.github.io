06b8a645dc50a8d9e085613b06b6323dbae679a0
Flowe4 <135275151+Flowe4@users.noreply.github.com>
2023-11-10 23:21:39 +0200

    code

---
+++ functions.js
@@ -0,0 +1,20 @@
+console.log("fuck you get out of inspect");
+
+let name = "fuck my dic"
+let age = 21
+
+name += 4
+
+
+console.log(name)
+console.log(age)
+
+
+let user;
+
+document.getElementById("Button").onclick = function() {
+    user = document.getElementById("Text").value;
+    console.log(user)
+    document.getElementById("Label").innerHTML = "Hello " + user;
+    check
+}
